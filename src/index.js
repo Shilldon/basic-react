babd4f7553af6103c29e4e9e8d6a28a119e3c68f
Shilldon <stewart.s-hill@ntlworld.com>
2021-02-25 22:14:14 +0000

    added function to flip card and changed card declaration to class

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Board = () => {
+
   const gameBoard = [];
 
     for (let i=0;i<=4;i++) {
@@ -11,23 +12,46 @@ const Board = () => {
 
   return (
     <div className="game-board d-flex flex-row">
-      {gameBoard}
+     {gameBoard} 
     </div>
   )
 }
 
+
 function renderCard(i) {
   return (
     <Card 
-      key={i}
+      key = {i}
     />
   )
 }
 
-function Card() {
-  return (
-    <div className="game-card"></div>
-  )
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      onClick: () => { this.cardClick() },
+      flipped: false,
+    }
+  
+  }
+
+  render() {
+    let flippedStatus = "unflipped-card"
+    if(this.state.flipped) {
+      flippedStatus = "flipped-card";
+    }
+    return (
+      <div className={"game-card "+flippedStatus} onClick={this.state.onClick}></div>
+    )
+  }
+
+  cardClick() {
+    this.setState({flipped: !this.state.flipped})   
+  }
 }
+
+
+
 
 ReactDOM.render(<Board />, document.getElementById('root'));
